7aba73a6a5d46b00bd0ae0cee05e18ff95f0c083
Nguyen Duong Trong Tin <tinht5667@gmail.com>
2026-04-21 22:45:51 +0700

    router link

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <template>
-  <router-view></router-view>
+  <header>
+    <the-header></the-header>
+  </header>
+
+  <main>
+    <router-view></router-view>
+  </main>
+
+  <footer></footer>
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+
 export default {
   name: 'App',
+  components: {
+    TheHeader
+  }
 }
 </script>
 
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="p-3 mb-3 bg-dark text-white">
+    <ul class="d-flex justify-content-center align-items-center">
+      <li>
+        <router-link class="mx-3" to="/users">Users</router-link>
+      </li>
+      <li>
+        <router-link class="mx-3" to="/pets">Pets</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+ul {
+  list-style: none;
+}
+
+a,
+a:hover {
+  color: unset;
+  text-decoration: none;
+}
+</style>
