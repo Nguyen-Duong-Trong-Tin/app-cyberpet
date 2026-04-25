edf981903b602d6f1af73c3260dc964fcb040863
Nguyen Duong Trong Tin <tinht5667@gmail.com>
2026-04-25 21:52:07 +0700

    page not found

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1 class="text-3xl font-bold text-center mt-10">404 - Not Found</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import { createRouter, createWebHistory } from "vue-router";
 import PetList from "./pages/PetList.vue";
 import UserList from "./pages/UserList.vue";
 import UserDetail from "./pages/UserDetail.vue";
+import NotFound from "./pages/NotFound.vue";
 
 const routes = [
   // { path: '/', redirect: '/users' },
   { path: '/users', component: UserList, alias: '/' },
   { path: '/user-detail', component: UserDetail },
   { path: '/pets', component: PetList },
+  { path: '/:pathMatch(.*)*', component: NotFound },
 ];
 const router = createRouter({
   history: createWebHistory(),
